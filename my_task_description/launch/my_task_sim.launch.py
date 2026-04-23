07d3c905a98421bea64d6fae8388ef8829e7e949
youlisu527 <suyouli527@outlook.com>
2026-04-23 18:59:52 +0800

    参数调整

--- my_task_description/launch/my_task_sim.launch.py
+++ my_task_description/launch/my_task_sim.launch.py
@@ -64,26 +64,26 @@ def generate_launch_description():
     with open(mj_dual_path, 'r') as f:
         mj_dual_text = f.read()
     
-    # Inject left arm virtual camera into the left hand body
+
     mj_dual_text = re.sub(
         r'(<body name="mj_left_hand"[^>]*>)',
         r'\1\n                        <camera name="left_arm_cam" pos="0 0.104765 0.053574" xyaxes="0 -1 0 -0.676 0 -0.736" fovy="58"/>',
         mj_dual_text
     )
-    # Inject right arm virtual camera into the right hand body
+
     mj_dual_text = re.sub(
         r'(<body name="mj_right_hand"[^>]*>)',
         r'\1\n                        <camera name="right_arm_cam" pos="0 0.104765 0.053574" xyaxes="0 -1 0 -0.676 0 -0.736" fovy="58"/>',
         mj_dual_text
     )
 
-    # Mount camera_part1 with identity transform w.r.t. left flange frame
+ 
     mj_dual_text = re.sub(
         r'(<site name="mj_left_flange_site"[^>]*/>)',
         r'\1\n                        <body name="camera_part1" pos="0 0 0" quat="0 0.93969262 0.34202014 0">\n                          <geom type="mesh" mesh="cam1" material="cam_mat" mass="0.5" contype="0" conaffinity="0"/>\n                        </body>',
         mj_dual_text
     )
-    # Mount camera_part2 with identity transform w.r.t. right flange frame
+  
     mj_dual_text = re.sub(
         r'(<site name="mj_right_flange_site"[^>]*/>)',
         r'\1\n                        <body name="camera_part2" pos="0 0 0" quat="0 0.93969262 0.34202014 0">\n                          <geom type="mesh" mesh="cam2" material="cam_mat" mass="0.5" contype="0" conaffinity="0"/>\n                        </body>',
